561ba7e07038f5006c5b48195aa0c7e601131a68
Owen Boyd <owen.boyd@D2L.com>
2023-11-01 14:40:05 -0400

    adding function get config file based on sample or dev layer. Replacing vars with let

--- RateLimitFileFactory.js
+++ RateLimitFileFactory.js
@@ -1,15 +1,33 @@
-var ApplicationRateLimitFile = require('./Models/ApplicationRateLimitFile');
-var InstanceRateLimitFile = require('./Models/InstanceRateFile');
+let ApplicationRateLimitFile = require('./Models/ApplicationRateLimitFile');
+let InstanceRateLimitFile = require('./Models/InstanceRateFile');
+const fs = require('fs');
+
+const DevFileName = "./Config.dev.json";
+
+function GetConfigFile()
+{
+	let targetFileName = "./Config.json";
+
+	if(fs.existsSync(targetFileName))
+		targetFileName = DevFileName;
+
+	let configFile = require(targetFileName);
+
+	return configFile;
+	
+}
 
 function CreateRateLimitObject(rateType)
 {
+	let configFile = GetConfigFile();
+
 	if(rateType == 'appid')
 	{
-		return new ApplicationRateLimitFile('bucket//rate-limiting-default-bucket.feature.json');
+		return new ApplicationRateLimitFile(configFile.bucketDirectory + 'rate-limiting-default-bucket.feature.json');
 	}
 	else if (rateType == 'instanceid')
 	{
-		return new InstanceRateLimitFile('bucket//rate-limiting-bucket-rate-limit.feature.json');
+		return new InstanceRateLimitFile(configFile.bucketDirectory + 'rate-limiting-bucket-rate-limit.feature.json');
 	}
 	else
 		return null;
